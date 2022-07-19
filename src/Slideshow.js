166a4deaaf7eaa025b966d79772ffc6563909f2e
Susan <susan23codes@gmail.com>
2022-07-19 16:35:21 -0400

    Added height to description so card doesn't jump

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,41 +1,43 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 export default function Slideshow(props) {
 
-    const {films} = props
+    const { films } = props
 
     const [currentFilm, setCurrentFilm] = useState(films[0])
     const [currentFilmIndex, setCurrentFilmIndex] = useState(0)
 
     // if whatever is in the end brackets changes then the code in the {} runs every 
     // time it changes.  If [] are empty, code just runs once, like in the fetch
-    useEffect(()=>{
+    useEffect(() => {
         setCurrentFilm(films[currentFilmIndex])
-    },[films, currentFilmIndex])
-    
-    
+    }, [films, currentFilmIndex])
+
+
     return (
         <>
             <div className="movieCard">
-        {/* currentFilm becomes the film at currentFilmIndex in useEffect above and
+                {/* currentFilm becomes the film at currentFilmIndex in useEffect above and
         the currentFilmIndex is set in the onClick event in the buttons */}
-                <h2>{currentFilm.title}</h2>
-                <p>{currentFilm.original_title}</p>
-                <p>{currentFilm.release_date}</p>
-                <p>{currentFilm.description}</p>
-                <img className="image" src={currentFilm.image}  alt="movie poster"/>
+                    <h2>{currentFilm.title}</h2>
+                    <p>{currentFilm.original_title}</p>
+                    <p>{currentFilm.release_date}</p>
+                <div className="description">
+                    <p>{currentFilm.description}</p>
+                </div>
+                    <img className="image" src={currentFilm.image} alt="movie poster" />
 
-                <p>{currentFilmIndex}</p>
-                {/* disabled = true when whatever condition in the {} is met */}
-                <button onClick={() => {setCurrentFilmIndex(0)}} 
-                    disabled={currentFilmIndex===0}>Start Over</button>
-                <button onClick={() => {setCurrentFilmIndex(currentFilmIndex - 1)}}
-                    disabled={currentFilmIndex===0}>Back</button>
-                <button onClick={() => {setCurrentFilmIndex(currentFilmIndex + 1)}}
-                    disabled={currentFilmIndex===8}>Next</button>
+                    <p>You are on Slide number {currentFilmIndex}</p>
+                    {/* disabled = true when whatever condition in the {} is met */}
+                    <button onClick={() => { setCurrentFilmIndex(0) }}
+                        disabled={currentFilmIndex === 0}>Start Over</button>
+                    <button onClick={() => { setCurrentFilmIndex(currentFilmIndex - 1) }}
+                        disabled={currentFilmIndex === 0}>Back</button>
+                    <button onClick={() => { setCurrentFilmIndex(currentFilmIndex + 1) }}
+                        disabled={currentFilmIndex === films.length - 1}>Next</button>
             </div>
         </>
     )
-    
+
 }
