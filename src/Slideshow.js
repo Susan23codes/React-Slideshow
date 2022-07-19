758393261aaca626b7dfd2acf1b150da2fb5b025
Susan <susan23codes@gmail.com>
2022-07-19 11:29:42 -0400

    changed how I disabled buttons

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -10,17 +10,6 @@ export default function Slideshow(props) {
     const [currentFilmIndex, setCurrentFilmIndex] = useState(0)
 
     useEffect(()=>{
-        if (currentFilmIndex == 0) {
-            document.getElementById("goback").disabled = true
-        } else {
-            document.getElementById("goback").disabled = false
-        }
-        if (currentFilmIndex == 8) {
-            document.getElementById("next").disabled = true
-        } else {
-            document.getElementById("next").disabled = false
-        }
-        
         setCurrentFilm(films[currentFilmIndex])
     },[films, currentFilmIndex])
     
@@ -35,9 +24,13 @@ export default function Slideshow(props) {
                 <img className="image" src={currentFilm.image}  alt="movie poster"/>
 
                 <p>{currentFilmIndex}</p>
-                <button onClick={() => {setCurrentFilmIndex(0)}}>Start Over</button>
-                <button id="goback" onClick={() => {setCurrentFilmIndex(currentFilmIndex - 1)}}>Back</button>
-                <button id="next"onClick={() => {setCurrentFilmIndex(currentFilmIndex + 1)}}>Next</button>
+                {/* disabled= true when whatever condition in the {} is met */}
+                <button onClick={() => {setCurrentFilmIndex(0)}} 
+                    disabled={currentFilmIndex===0}>Start Over</button>
+                <button onClick={() => {setCurrentFilmIndex(currentFilmIndex - 1)}}
+                    disabled={currentFilmIndex===0}>Back</button>
+                <button onClick={() => {setCurrentFilmIndex(currentFilmIndex + 1)}}
+                    disabled={currentFilmIndex===8}>Next</button>
             </div>
         </>
     )
